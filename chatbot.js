300ac3eec74a16854ed4ded68030ca6446492207
Leron <leroner@wcnet.whitecase.com>
2019-04-05 22:25:14 +0800

    Initial version.

---
+++ chatbot.js
@@ -0,0 +1,32 @@
+function synchronize() {
+
+    var jsonData = {
+        "add": {
+            "qnaList":[
+                {"source": "Custom"},
+                {"answer": "Hello"},
+                {"questions": ["Hi", "Hello"]}
+            ],
+        },
+        "delete": {
+            "sources": ["Custom"]
+        },
+        "update": {}
+    }   
+
+    var request = new XMLHttpRequest();    
+    var parameters = {
+        "kbId": "{MY_KB_ID}",
+                "body": jsonData
+    }
+    request.open("POST", "https://qnawcfaq.azurewebsites.net/qnamaker/knowledgebases/{kbId}}/generateAnswer", true);                    
+    request.setRequestHeader("Authorization", "EndpointKey {key}}");
+    request.setRequestHeader("Content-type", "application/json");                    
+    request.onreadystatechange = function () { //Call a function when the state changes.
+        if (request.readyState == 4 && request.status == 200) {
+            alert(request.responseText);
+        }
+    }
+
+    request.send(JSON.stringify(parameters));
+}
